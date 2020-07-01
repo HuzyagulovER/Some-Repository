88555f8d0a4b2fe795e52d443aa657b2d17cb6d9
Eduard <eduard.huzyagulov@mail.ru>
2020-07-02 02:05:42 +0500

    Uploaded 02.07.2020  1:31:04,05

--- BC.js
+++ BC.js
@@ -3,12 +3,14 @@
 let mainNumber = prompt("Введите 4-хзначное число без повторяющихся цифр:");
 let checkNumber = prompt("Введите проверочное число:");
 
-if (getCategoryOfNumber(mainNumber) !== getCategoryOfNumber(checkNumber)) {
-	alert("Число содержит неверное количество цифр!");
-};
+alert(searchOfCoinsedence(mainNumber, checkNumber));
 
-if (searchRepeatingNumbers(mainNumber)) {
-	alert("Присутствуют повторяющиеся цифры!");
+if ((getCategoryOfNumber(mainNumber) !== getCategoryOfNumber(checkNumber)) 
+	|| (searchRepeatingNumbers(mainNumber))
+	|| (searchRepeatingNumbers(checkNumber))) {
+	alert("Число содержит неверное количество цифр!");
+} else {
+	
 };
 
 function searchRepeatingNumbers(number) {
@@ -62,10 +64,10 @@ function deleteLastNumber(number) {
 	return Math.floor(number/10);
 };
 
-function searchOfBulls(mainNumber, checkNumber) {
+function searchOfCoinsedence(mainNumber, checkNumber) {
 	let copyCheckNumber = checkNumber;
 	let copyMainNumber = mainNumber;
-	let numberOfBulls = 0;
+	let numberOfCoinsedence = 0;
 
 	for ( ; copyCheckNumber > 0; ) {
 
@@ -78,15 +80,32 @@ function searchOfBulls(mainNumber, checkNumber) {
 			copyMainNumber = deleteLastNumber(copyMainNumber);
 		};
 
-		copyCheckNumber = deleteLastNumber(checkNumber);
+		copyCheckNumber = deleteLastNumber(copyCheckNumber);
 		copyMainNumber = mainNumber;
+	};
+
+	return numberOfCoinsedence;
+};
+
+function searchOfBulls(mainNumber, checkNumber) {
+	let copyCheckNumber = checkNumber;
+	let copyMainNumber = mainNumber;
+	let numberOfBulls = 0;
+
+	for ( ; copyMainNumber > 0; ) {
+
+		if (getLastNumber(copyCheckNumber) === getLastNumber(copyMainNumber)) {
+			numberOfBulls++;
+		};
+
+		copyMainNumber = deleteLastNumber(copyMainNumber);
+		copyCheckNumber = deleteLastNumber(copyCheckNumber);
 	};
 
 	return numberOfBulls;
 };
 
 function searchOfCows(mainNumber, checkNumber) {
-	
-}
-
-alert(searchOfBulls(mainNumber, checkNumber));
+	let numberOfCows = searchOfCoinsedence(mainNumber, checkNumber) - searchOfBulls(mainNumber, checkNumber);
+	return numberOfCows;
+};
