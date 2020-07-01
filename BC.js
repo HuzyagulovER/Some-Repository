48df38f5fbfc4f38ff9600731100559b63c3de01
Eduard <eduard.huzyagulov@mail.ru>
2020-07-01 23:43:34 +0500

    Uploaded 01.07.2020 23:43:34,68

--- BC.js
+++ BC.js
@@ -1,1 +1,20 @@
-asdasd
+"use strict";
+
+/*let mainNumber = prompt("Введите 4-хзначное число без повторяющихся цифр:");
+let checkNumber = prompt("Введите проверочное число:");
+*/
+
+function getLastNumber(number) {
+	let lastNumber = (number/10-Math.trunc(number/10))*10;
+	return lastNumber;
+};
+
+let a = 1578/10;
+let b = Math.trunc(a);
+let c = a - b;
+alert(b);
+// alert(getLastNumber(12));
+
+function searchOfBulls(number) {
+	// body...
+};
