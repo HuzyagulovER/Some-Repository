d077496110c071b1cb60321cdde6625ed1aa403e
Eduard <eduard.huzyagulov@mail.ru>
2020-07-02 22:39:49 +0500

    Uploaded 02.07.2020 22:39:49,40

--- BC.js
+++ BC.js
@@ -1,18 +1,37 @@
 "use strict";
 
-let mainNumber = prompt("Введите 4-хзначное число без повторяющихся цифр:");
-let checkNumber = prompt("Введите проверочное число:");
+let mainNumber = prompt("Введите 4-хзначное число без повторяющихся цифр:", 1234);
+// let checkNumber = prompt("Введите проверочное число:");
 
-if ((getCategoryOfNumber(mainNumber) !== getCategoryOfNumber(checkNumber)) 
+alert(generateWithout(mainNumber));
+
+/*if ((mainNumber != Number(mainNumber))
+		|| (checkNumber != Number(checkNumber))) {
+
+		alert("Введены не числа!");
+
+} else {
+
+	if ((getCategoryOfNumber(mainNumber) !== getCategoryOfNumber(checkNumber))
 	|| (searchRepeatingNumbers(mainNumber))
 	|| (searchRepeatingNumbers(checkNumber))) {
-	alert("Число содержит неверное количество цифр!");
-} else {
-	alert(searchOfCoinsedence(mainNumber, checkNumber));
-	alert(searchOfBulls(mainNumber, checkNumber));
-	alert(searchOfCows(mainNumber, checkNumber));
-};
 
+	alert("Число содержит неверное количество цифр!");
+
+	} else {
+		alert(quantityOfCoinсedence(mainNumber, checkNumber));
+		alert(searchCoincedence(mainNumber, checkNumber));
+	
+		alert(quantityOfBulls(mainNumber, checkNumber));
+		alert(quantityOfCows(mainNumber, checkNumber));
+	};
+};*/
+
+
+/*	alert(searchCoincedence(mainNumber, checkNumber));
+	
+	alert(quantityOfBulls(mainNumber, checkNumber));
+	alert(quantityOfCows(mainNumber, checkNumber));*/
 
 
 function searchRepeatingNumbers(number) {
@@ -39,6 +58,7 @@ function searchRepeatingNumbers(number) {
 	return false;
 };
 
+
 function getLastNumber(number) {
 	let variable = number/10;
 	let variable2 = Math.trunc(variable);
@@ -47,6 +67,7 @@ function getLastNumber(number) {
 
 	return lastNumber;
 };
+
 
 function getCategoryOfNumber(number) {
 	let i = 0;
@@ -62,21 +83,22 @@ function getCategoryOfNumber(number) {
 	return i;
 };
 
+
 function deleteLastNumber(number) {
 	return Math.floor(number/10);
 };
 
-function searchOfCoinsedence(mainNumber, checkNumber) {
+
+function searchCoincedence(mainNumber, checkNumber) {
 	let copyCheckNumber = checkNumber;
 	let copyMainNumber = mainNumber;
-	let numberOfCoinsedence = 0;
 
 	for ( ; copyCheckNumber > 0; ) {
 
 		for ( ; copyMainNumber > 0; ) {
 
 			if (getLastNumber(copyCheckNumber) === getLastNumber(copyMainNumber)) {
-				numberOfCoinsedence++;
+				return true;
 			};
 
 			copyMainNumber = deleteLastNumber(copyMainNumber);
@@ -86,28 +108,98 @@ function searchOfCoinsedence(mainNumber, checkNumber) {
 		copyMainNumber = mainNumber;
 	};
 
-	return numberOfCoinsedence;
+	return false;
 };
 
-function searchOfBulls(mainNumber, checkNumber) {
+
+function quantityOfCoinсedence(mainNumber, checkNumber) {
 	let copyCheckNumber = checkNumber;
 	let copyMainNumber = mainNumber;
-	let numberOfBulls = 0;
+	let coinсedenceQuantity = 0;
+
+	for ( ; copyCheckNumber > 0; ) {
+
+		for ( ; copyMainNumber > 0; ) {
+
+			if (getLastNumber(copyCheckNumber) === getLastNumber(copyMainNumber)) {
+				coinсedenceQuantity++;
+			};
+
+			copyMainNumber = deleteLastNumber(copyMainNumber);
+		};
+
+		copyCheckNumber = deleteLastNumber(copyCheckNumber);
+		copyMainNumber = mainNumber;
+	};
+
+	return coinсedenceQuantity;
+};
+
+
+function quantityOfBulls(mainNumber, checkNumber) {
+	let copyCheckNumber = checkNumber;
+	let copyMainNumber = mainNumber;
+	let quantityOfBulls = 0;
 
 	for ( ; copyMainNumber > 0; ) {
 
 		if (getLastNumber(copyCheckNumber) === getLastNumber(copyMainNumber)) {
-			numberOfBulls++;
+			quantityOfBulls++;
 		};
 
 		copyMainNumber = deleteLastNumber(copyMainNumber);
 		copyCheckNumber = deleteLastNumber(copyCheckNumber);
 	};
 
-	return numberOfBulls;
+	return quantityOfBulls;
 };
 
-function searchOfCows(mainNumber, checkNumber) {
-	let numberOfCows = searchOfCoinsedence(mainNumber, checkNumber) - searchOfBulls(mainNumber, checkNumber);
-	return numberOfCows;
+
+function quantityOfCows(mainNumber, checkNumber) {
+	let cowsQuantity = quantityOfCoinсedence(mainNumber, checkNumber) - quantityOfBulls(mainNumber, checkNumber);
+	return cowsQuantity;
+};
+
+
+function generateRandom() {
+	let returnedNumber = 0;
+	let indicator;
+
+	while (getCategoryOfNumber(returnedNumber) !== 4) {
+		let newNumber = Math.floor( Math.random() * 10 );
+
+		indicator = searchCoincedence(returnedNumber, newNumber);
+
+		if (!(indicator)) {
+
+			returnedNumber = returnedNumber * 10 + newNumber;
+
+		};
+	};
+
+	return returnedNumber;
+
+};
+
+
+function generateWithout(cuttedDigits) {
+	let returnedNumber = 0;
+	let indicator;
+	let newNumber = 1;
+
+	while (getCategoryOfNumber(returnedNumber) !== 4) {
+
+		indicator = searchCoincedence(cuttedDigits, newNumber);
+
+		if (!(indicator)) {
+
+			returnedNumber = returnedNumber * 10 + newNumber;
+
+		};
+
+		newNumber++;
+	};
+
+	return returnedNumber;
+
 };
