1c13749105ce2d8b17055acae4167e62c1f2717f
Eduard <eduard.huzyagulov@mail.ru>
2020-07-21 11:35:46 +0500

    Uploaded 21.07.2020 11:35:46,18

--- BC.js
+++ BC.js
@@ -1,10 +1,2 @@
 "use strict";
 
-function truncate(str, maxlength) {
-
-	if (str.length > maxlength) return (str.slice(0, maxlength - 3) + "...");
-	return str;
-
-};
-
-alert(truncate("Всем привет!", 20));
