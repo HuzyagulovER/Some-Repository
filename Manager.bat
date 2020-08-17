f11e58920a68426993a9a5e8cf2da0b6101e38f4
Eduard <eduard.huzyagulov@mail.ru>
2020-08-17 13:11:20 +0500

    Uploaded 17.08.2020 13:11:20,00

--- Manager.bat
+++ Manager.bat
@@ -1,5 +1,5 @@
 @echo off
-echo ^<Command list^>: act ^| push ^| restore ^| exit
+echo ^<Command list^>: act ^| push ^| restore ^| reload ^| exit
 
 :Loop
 	goto Manage_Menu
@@ -9,7 +9,14 @@ echo ^<Command list^>: act ^| push ^| restore ^| exit
 	set /p cmd=^<Enter command^>:
 	if not defined cmd goto Manage_Menu
 	if %cmd%==null goto Manage_Menu
-	if /i %cmd%==act (start Bat\Activation_For_TR.bat & goto Loop)
-	if /i %cmd%==push (start Bat\Push_For_TR.bat & goto Loop)
+	if /i %cmd%==act (start Bat\Activation_For_TR.bat & goto Manage_Menu)
+	if /i %cmd%==push (start Bat\Push_For_TR.bat & goto Manage_Menu)
 	if /i %cmd%==exit (exit)
+	if /i %cmd%==reload (start Manager.bat & exit)
+	call :Colors
+	goto Manage_Menu
 	pause
+
+:Colors
+	powershell write-host -fore Red "%cmd% is undefined command!"
+	exit /b
