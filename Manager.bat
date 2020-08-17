6c3460878dc5b39bb70261ab42349b5c99ff11fe
Eduard <eduard.huzyagulov@mail.ru>
2020-08-17 11:59:25 +0500

    Uploaded 17.08.2020 11:59:25,14

--- Manager.bat
+++ Manager.bat
@@ -5,8 +5,9 @@ echo ^<Command list^>: act ^| push ^| restore ^| exit
 	goto Manage_Menu
 
 :Manage_Menu
-	set /p cmd=^<Enter command^>: 
+	set /p cmd=^<Enter command^>:
+	if not defined cmd goto Manage_Menu 
 	if /i %cmd%==act (start Bat\Activation_For_TR.bat & goto Loop)
-	if /i %cmd%==Push (start Bat\Push_For_TR.bat & goto Loop)
+	if /i %cmd%==push (start Bat\Push_For_TR.bat & goto Loop)
 	if /i %cmd%==exit (exit)
 	pause
