f8127e0972cad1dfbdec4b25d0edeb26d9c64d19
Eduard <eduard.huzyagulov@mail.ru>
2020-08-17 12:38:58 +0500

    Uploaded 17.08.2020 12:38:57,84

--- Manager.bat
+++ Manager.bat
@@ -5,8 +5,10 @@ echo ^<Command list^>: act ^| push ^| restore ^| exit
 	goto Manage_Menu
 
 :Manage_Menu
+	set cmd=
 	set /p cmd=^<Enter command^>:
-	if not defined cmd goto Manage_Menu 
+	if not defined cmd goto Manage_Menu
+	if %cmd%==null goto Manage_Menu
 	if /i %cmd%==act (start Bat\Activation_For_TR.bat & goto Loop)
 	if /i %cmd%==push (start Bat\Push_For_TR.bat & goto Loop)
 	if /i %cmd%==exit (exit)
