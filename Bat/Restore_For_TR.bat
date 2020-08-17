3b12800242bd2ec3ba16d8922b3c66ad0516fde2
Eduard <eduard.huzyagulov@mail.ru>
2020-08-17 20:28:03 +0500

    Uploaded 17.08.2020 20:28:03,02

--- Bat/Restore_For_TR.bat
+++ Bat/Restore_For_TR.bat
@@ -10,13 +10,13 @@ goto MAIN
 	exit /b
 
 :MAIN
-::	git restore .
+	git restore .
  	git status
 	echo.
 	call :Colors "Black" "DarkYellow" "The last commit message:   %message%"
 	echo.
 	call :Colors "Yellow" "DarkGreen" "The files have been restored successfully!"
-::	timeout /t 3
+	timeout /t 3
 	pause
 	exit
 
