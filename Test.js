c10e5818923021b629d4e96398536f4068deec44
Eduard <eduard.huzyagulov@mail.ru>
2020-08-19 22:36:28 +0500

    Uploaded 19.08.2020 22:36:27,77

--- Test.js
+++ Test.js
@@ -1,2 +1,18 @@
 "use strict";
 
+let btn = document.getElementById("css8_btn");
+let p = document.getElementById("css8_p");
+
+let i = 0;
+p.innerHTML = "Lol"
+
+btn.onclick = function() {
+	if (i==0) {
+		p.style.display = "block";
+		i=1;
+	} else {
+		p.style.display = "none";
+		i=0;
+	};
+
+};
