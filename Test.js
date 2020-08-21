584bec9ac80d04d65fabc64579120d4b063746b1
Eduard <eduard.huzyagulov@mail.ru>
2020-08-21 14:39:32 +0500

    Uploaded 21.08.2020 14:39:32,33

--- Test.js
+++ Test.js
@@ -1,10 +1,7 @@
 "use strict";
 
-let btn = document.getElementById("css8_btn");
-let p = document.getElementById("css8_p");
-let i=0;
-
-btn.onclick = function() {
-	this.innerHTML = i;
-	i+=1;
-};
+$(document).ready( function() {
+	$('.menu_burger').click( function(event) {
+		$('.menu_burger, .menu_burger__line').toggleClass('active');
+	});
+});
